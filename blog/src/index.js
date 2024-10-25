b7bb0a2a354a05cffa32b8a5d6b52b936fc287aa
Sairam Venkatesh Konar <140108101+SURG30N29@users.noreply.github.com>
2024-10-25 08:00:57 +0530

    Delete blog directory

--- blog/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './App.css'; // Import global CSS
-
-ReactDOM.render(<App />, document.getElementById('root'));
